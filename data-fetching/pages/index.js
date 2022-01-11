f48c11e77041b6749dd8b3d5917c4d6186b3d0a5
Najmul-Kabir-Joy <najmulkabit178@gmail.com>
2022-01-11 18:22:41 +0600

    new fetch from root

--- data-fetching/pages/index.js
+++ data-fetching/pages/index.js
@@ -2,18 +2,20 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
+import fs from 'fs/promises';
+import path from 'path';
+function Home(props) {
+  const { products } = props;
+  // const [data, setData] = useState();
+  // useEffect(() => {
+  //   fetch('https://jsonplaceholder.typicode.com/posts')
+  //     .then(res => res.json())
+  //     .then(data => setData(data))
+  // }, [])
 
-export default function Home() {
-  const [data, setData] = useState();
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(res => res.json())
-      .then(data => setData(data))
-  }, [])
-  console.log(data);
   return (
     <>
-      <ul>
+      {/* <ul>
         {
           data?.map(e => {
             return (
@@ -21,7 +23,26 @@ export default function Home() {
             )
           })
         }
-      </ul>
+      </ul> */}
+      {
+        products.map(e => <h1 key={e.id}>{e.title}</h1>)
+      }
     </>
   )
 }
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return {
+    props: {
+      products: data.products,
+    },
+    //WILL WORK IN PRODUCTTION THIS IS CALL INCREMENTAL STATIC GENERATION (ISR);
+    revalidate: 10
+  }
+}
+
+export default Home;
